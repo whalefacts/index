05e8caea41ffc2766f0394df2b3926085e5b926c
amby <89108711+ppleatr@users.noreply.github.com>
2022-01-21 17:49:00 +0100

    Update whalefacts.js

--- whalefacts.js
+++ whalefacts.js
@@ -5,7 +5,9 @@ const factElement = document.getElementById('fact');
 const whale_facts = [
 	"Whales have 3 horns on their tails.",
 	"There are up to 26 spiders in a whale’s mouth a year, and you're next.",
-	"Sperm whales can't cum."
+	"Sperm whales can't cum.",
+	"Ronald McDonald was named after a beluga whale who got in the Guinness world records for “Largest Whale Penis” recording in at a whopping 13 inn HES."
+	
 ]
 
 function minMaxRandom(min, max) {
